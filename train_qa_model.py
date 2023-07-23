d5b31a676c4ecb61fbb00aa000fae5b61b939ee8
Vkwah <vk17wah@gmail.com>
2023-07-23 13:54:53 +0530

    second

--- train_qa_model.py
+++ train_qa_model.py
@@ -1,53 +1,43 @@
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+import numpy as np
 
-# Create a TF-IDF vectorizer and Logistic Regression classifier
-vectorizer = TfidfVectorizer()
-classifier = LogisticRegression()
+def train_online_model(initial_model, vectorizer):
+    while True:
+        user_input = input("User: ").strip()
+        if user_input.lower() == "exit":
+            break
+        
+        X_user = vectorizer.transform([user_input])
+        y_user = int(input("Label (0 for negative, 1 for positive): ").strip())
+        
+        initial_model.partial_fit(X_user, [y_user], classes=[0, 1])
 
-# Sample training data (you should replace these with your own data)
-texts = [
-    "Transformers is a deep learning model introduced in the paper 'Attention Is All You Need'.",
-    "It has gained popularity for various natural language processing tasks.",
-    "BERT is another popular model used for question answering.",
-    "It stands for Bidirectional Encoder Representations from Transformers.",
-    "The BERT model has been fine-tuned on various tasks including question answering."
-]
-
-# Corresponding labels (0 for non-answer, 1 for answer)
-labels = [1, 0, 1, 0, 1]
-
-# Transform the text data into numerical features
-X = vectorizer.fit_transform(texts)
-
-# Convert labels to NumPy array
-y = np.array(labels)
-
-# Train the classifier on the data
-classifier.fit(X, y)
-
-#part 2
+        print("Model has been updated based on your input.")
 
 if __name__ == "__main__":
-    # Example questions
-    questions = [
-        "What is Transformers?",
-        "What is the paper 'Attention Is All You Need' about?",
-        "For which tasks is BERT commonly used?",
-        "What does BERT stand for?",
-        "Has the BERT model been fine-tuned on various tasks?"
+    # Sample existing dataset (you should replace these with your own data)
+    texts = [
+        "I love this product!",
+        "This is terrible.",
+        "Great service!",
+        "Awful experience.",
+        "Highly recommended.",
+        "I would never buy this again.",
     ]
 
-    # Transform the questions into numerical features using the same vectorizer
-    X_test = vectorizer.transform(questions)
+    # Corresponding labels (0 for negative sentiment, 1 for positive sentiment)
+    labels = [1, 0, 1, 0, 1, 0]
 
-    # Make predictions for the questions
-    predictions = classifier.predict(X_test)
+    # Create a TF-IDF vectorizer and train the initial model
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(texts)
+    y = np.array(labels)
 
-    # Print the answers based on the predictions
-    for i, question in enumerate(questions):
-        answer = "Yes" if predictions[i] == 1 else "No"
-        print(f"Question: {question}")
-        print(f"Answer: {answer}\n")
+    initial_model = SGDClassifier(loss='log', random_state=42)  # Log-loss for probabilistic output
+    initial_model.fit(X, y)
 
+    print("Initial Model Training Completed.")
+
+    # Start online training and reply loop
+    train_online_model(initial_model, vectorizer)
